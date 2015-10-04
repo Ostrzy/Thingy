dd0e25146c0d68210be772f1334fc2b8664c6e77
Michał Simka <michal.simka@ostrzy.net>
2015-10-05 00:07:57 +0200

    Use new helpers in systems
    Code is much cleaner and easy to understand now.

--- lib/systems/hunger.ex
+++ lib/systems/hunger.ex
@@ -2,19 +2,10 @@ defmodule System.Hunger do
   @behaviour GameSystem
 
   def run(entities) do
-    applicable_entities = Enum.filter entities, fn entity ->
-      Entity.contains?(entity, Component.Hunger)
-    end
-
-    Enum.each applicable_entities, &hunger/1
+    entities
+    |> Enum.filter(fn entity -> Entity.contains?(entity, Component.Hunger) end)
+    |> Enum.each(fn entity -> Component.Hunger.famish(entity, 1) end)
 
     entities
-  end
-
-  defp hunger(entity) do
-    hunger_state = Entity.get_state(entity, Component.Hunger)
-    hunger = Enum.max([hunger_state.hunger - 1, -hunger_state.max_hunger])
-    hunger_state = %{hunger_state | hunger: hunger}
-    Entity.set_state(entity, Component.Hunger, hunger_state)
   end
 end
