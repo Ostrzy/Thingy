6c6ef603ad7e3bc598fe3267ac73e8df26aedc39
Michał Simka <michal.simka@ostrzy.net>
2015-10-05 11:22:24 +0200

    Use new helpers in System.Starvation

--- lib/systems/starvation.ex
+++ lib/systems/starvation.ex
@@ -2,20 +2,11 @@ defmodule System.Starvation do
   @behaviour GameSystem
 
   def run(entities) do
-    applicable_entities = Enum.filter entities, fn entity ->
-      Entity.contains?(entity, [Component.Hunger, Component.Health])
-    end
-
-    Enum.each(applicable_entities, &starvation/1)
     entities
-  end
+    |> Enum.filter(fn entity -> Entity.contains?(entity, [Component.Hunger, Component.Health]) end)
+    |> Enum.filter(fn entity -> Component.Hunger.hungry?(entity) end)
+    |> Enum.each(fn entity -> Component.Health.damage(entity, 1) end)
 
-  defp starvation(entity) do
-    hunger_state = Entity.get_state(entity, Component.Hunger)
-    if hunger_state.hunger < 0 do
-      hp_state = Entity.get_state(entity, Component.Health)
-      hp = Enum.max([hp_state.hp - 1, 0])
-      Entity.set_state(entity, Component.Health, %{hp_state | hp: hp})
-    end
+    entities
   end
 end
