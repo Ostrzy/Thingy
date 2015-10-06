4be97540173298260934121a3fd68a4f38caea95
PLkolek <mateusz.kolaczek@gmail.com>
2015-10-06 18:44:04 +0200

    Reject instead of filter

--- lib/systems/death.ex
+++ lib/systems/death.ex
@@ -5,7 +5,7 @@ defmodule System.Death do
     dead_entities =
       entities
       |> Enum.filter(&Entity.contains? &1, Component.Health)
-      |> Enum.filter(&not Component.Health.alive? &1)
+      |> Enum.reject(&Component.Health.alive? &1)
 
     Enum.each dead_entities, &Agent.stop/1
 
