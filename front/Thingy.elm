f9f49e7f3ec93f7e2c47d34d7c4e8b6392ce5914
PLkolek <mateusz.kolaczek@gmail.com>
2015-10-28 19:31:17 +0100

    Read and show data from server

--- front/Thingy.elm
+++ front/Thingy.elm
@@ -1,3 +1,21 @@
-import Graphics.Element exposing (show)
+module Thingy where
 
-main = show "Hello world"
+import Graphics.Element exposing (show)
+import Task exposing (Task, andThen)
+import String
+import Time
+import SocketIO
+
+
+--One day, there will be a server writing to me...
+socket : Task x SocketIO.Socket
+socket = SocketIO.io "http://localhost:8001" SocketIO.defaultOptions
+
+received : Signal.Mailbox String
+received = Signal.mailbox "No data"
+
+-- set up the receiving of data
+port responses : Task x ()
+port responses = socket `andThen` SocketIO.on "" received.address
+
+main = Signal.map show received.signal
