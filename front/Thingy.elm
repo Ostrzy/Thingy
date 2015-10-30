dd330f44a8b27e9324a503a7b7d7fbeabe75a199
PLkolek <mateusz.kolaczek@gmail.com>
2015-10-30 19:32:32 +0100

    HOLY FUCK!

--- front/Thingy.elm
+++ front/Thingy.elm
@@ -4,18 +4,18 @@ import Graphics.Element exposing (show)
 import Task exposing (Task, andThen)
 import String
 import Time
-import SocketIO
+import WebSocket
 
 
 --One day, there will be a server writing to me...
-socket : Task x SocketIO.Socket
-socket = SocketIO.io "http://localhost:8001" SocketIO.defaultOptions
+socket : Task x WebSocket.WebSocket
+socket = WebSocket.create "ws://localhost:8001" received.address
 
-received : Signal.Mailbox String
-received = Signal.mailbox "No data"
+received : Signal.Mailbox WebSocket.SocketEvent
+received = Signal.mailbox <| WebSocket.Msg"No data"
 
 -- set up the receiving of data
-port responses : Task x ()
-port responses = socket `andThen` SocketIO.on "" received.address
+port responses : Task x WebSocket.WebSocket
+port responses = socket
 
 main = Signal.map show received.signal
---
+++ front/elm-stuff/packages/vilterp/elm-websockets/1.0.0/WebSocket.elm
@@ -0,0 +1,42 @@
+module WebSocket where
+
+import Task
+import Signal
+
+import Native.WebSocket
+
+
+type alias URL =
+  String
+
+
+type alias Message =
+  String
+
+
+type WebSocket =
+  WebSocket
+
+
+type SocketEvent
+  = Msg Message
+  | Close
+
+
+type InvalidStateError
+  = SocketNotOpenError
+
+
+create : URL -> Signal.Address SocketEvent -> Task.Task x WebSocket
+create =
+  Native.WebSocket.create
+
+
+send : WebSocket -> Message -> Task.Task InvalidStateError ()
+send =
+  Native.WebSocket.send
+
+
+close : WebSocket -> Task.Task x ()
+close =
+  Native.WebSocket.close
